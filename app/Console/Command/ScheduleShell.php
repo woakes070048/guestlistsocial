3a54330b419874dc44663c82ed0ef564077463d8
sharif9876 <sharif9876@hotmail.com>
2015-07-02 21:18:40 +0100

    daylight savings

--- app/Console/Command/ScheduleShell.php
+++ app/Console/Command/ScheduleShell.php
@@ -6,8 +6,8 @@ class ScheduleShell extends AppShell {
 	public $uses = array('TwitterAccount', 'CronTweet', 'Tweet');
 
     public function main() {
-        $lowerbound = time() - 120;
-        $upperbound = time();
+        $lowerbound = time() - 120 + 3600;
+        $upperbound = time() + 3600;
         $toTweet = $this->CronTweet->find('all', array('conditions' => array('timestamp <' => $upperbound, 'timestamp >' => $lowerbound)));
         $count = $this->CronTweet->find('count', array('conditions' => array('timestamp <' => $upperbound, 'timestamp >' => $lowerbound)));
         $toDelete = $this->CronTweet->find('list', array('fields' => array('id'), 'conditions' => array('timestamp <' => $upperbound, 'timestamp >' => $lowerbound)));
